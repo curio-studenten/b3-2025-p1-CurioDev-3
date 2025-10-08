e5fc9f32445205b8c90ab697b8e5d9399f813089
Hannes <rollemandufour@gmail.com>
2025-10-08 15:07:48 +0200

    Opmaak gefixt

--- index.php
+++ index.php
@@ -4,14 +4,21 @@
 <head>
     <title></title>
     <?php require_once 'head.php'; ?>
+    <link rel="stylesheet" href="css/style.css">
 </head>
+
+<header>
+    <div class="wrapper">
+        <nav>
+            <a href="incdex.php">Home</a>
+            <a href="tasks/create.php"></a>
+            <a href="tasks/overvieuw.php">Overvieuw</a>
+        </nav>
+    </div>
+</header>
 
 <body>
     
-    <div class="container">
-      
-    </div>
-
 </body>
 
 </html>
