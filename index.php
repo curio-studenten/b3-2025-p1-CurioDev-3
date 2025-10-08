30235e0f22a0a61fbb8f31bcbd55938d87ae623b
Hannes <rollemandufour@gmail.com>
2025-10-08 18:39:21 +0200

    navbar develepment

--- index.php
+++ index.php
@@ -4,14 +4,13 @@
 <head>
     <title></title>
     <?php require_once 'head.php'; ?>
-    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <header>
     <div class="wrapper">
         <nav>
-            <a href="incdex.php">Home</a>
-            <a href="tasks/create.php"></a>
+            <a href="index.php">Home</a>
+            <a href="tasks/create.php">takenlijst</a>
             <a href="tasks/overvieuw.php">Overvieuw</a>
         </nav>
     </div>
