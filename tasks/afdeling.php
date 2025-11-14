19c2c115a800a82a4b850b790c4260cc39aab0a9
BoredxManiac <Manicalsome.1@gmail.com>
2025-11-14 11:11:20 +0100

    afdeling.php aangemaakt

---
+++ tasks/afdeling.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php require_once '../head.php'; ?>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="../home.php">Home</a>
+            <a href="index.php">Takenoverzicht</a>
+        </nav>
+    </header>
+    <h1>Here are all the DONE tasks</h1>
+    <?php 
+        require_once '../backend/conn.php';
+
+        $query = "SELECT * FROM taken where status = 'done' ";
+
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        $taken = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        $deadlines = array_column($taken, 'deadline');
+
+        array_multisort($deadlines, SORT_ASC, $taken);
+
+        ?> 
+        <table>
+            <tr>
+
+                <th>Titel</th>
+                <th>Description</th>
+                <th>Department</th>
+                <th>Status</th>
+                <th>deadline</th>
+            </tr>
+            <?php foreach($taken as $taak):   ?>
+                <tr>
+
+            <td> <?php echo $taak['titel']; ?></td>
+            <td> <?php echo $taak['beschrijving']; ?></td>
+            <td> <?php echo $taak['afdeling']; ?></td>
+            <td> <?php echo $taak['status']; ?></td>
+            <td> <?php echo $taak['deadline']; ?></td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+</body>
+</html>
