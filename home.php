e210c4557d9c359128ba2d253e1c9e9a156dcdab
Hannes <rollemandufour@gmail.com>
2025-11-19 16:33:02 +0100

    homepage css added (still in progress)

--- home.php
+++ home.php
@@ -6,12 +6,19 @@
     <title>Homepage | Pretpark Webapp</title>
     <?php require_once 'head.php'; ?> <!-- Laadt o.a. CSS -->
 </head>
+
 <body>
     <?php require_once 'header.php' ?>
 
-    <main>
-        <h1>Welkom bij de Pretpark Webapp</h1>
-        <p>Beheer eenvoudig alle taken van het pretpark.</p>
-    </main>
+    <div class="wrapper">
+        <div class="h1_homepage">
+            <h1>Welkom bij de Pretpark Webapp</h1>
+        </div>
+        <div class="container_homepage">
+            <main>
+                <p>Beheer eenvoudig alle taken van het pretpark.</p>
+            </main>
+        </div>
+    </div>
 </body>
 </html>
